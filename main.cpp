7cd629b9132444451f98c953474dd1446e8cc323
Talha Asif <sp26-bse-064@cuilahore.edu.pk>
2026-09-12 19:34:29 +0500

    WSA Data

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <conio.h>
+#include <winsock2.h>
+#include <string>
 #include "titleScreen.cpp"
 using namespace std;
-
+#pragma comment(lib, "ws2_32.lib")
 int main()
 {
+    WSADATA wsaData;
+    SOCKET listenSocket = INVALID_SOCKET;
+    SOCKET clientSocket = INVALID_SOCKET;
+    try{
+        if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0){
+		    throw runtime_error("Error : " + to_string(WSAGetLastError()));
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
     title();
     getch();
 }
