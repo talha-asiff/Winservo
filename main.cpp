017fd192cd662c7493593ecd24f522b3d5843291
Talha Asif <sp26-bse-064@cuilahore.edu.pk>
2026-09-12 19:45:07 +0500

    socket creation, server config

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ using namespace std;
 #pragma comment(lib, "ws2_32.lib")
 int main()
 {
+    title();
     WSADATA wsaData;
     SOCKET listenSocket = INVALID_SOCKET;
     SOCKET clientSocket = INVALID_SOCKET;
+    sockaddr_in serverAddr; //struct to save all server configurations
     try{
         if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0){
 		    throw runtime_error("Error : " + to_string(WSAGetLastError()));
@@ -19,7 +21,16 @@ int main()
     {
         std::cerr << e.what() << '\n';
     }
-    
-    title();
+    listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    // IPPROTO_TCP = 6 : TCP protocol |||| If we put 0 here system automatically choose dafault protocol for that port
+    if (listenSocket == INVALID_SOCKET) {
+        cerr << "Socket creation failed: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return INVALID_SOCKET;
+    }
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY; // Listen on all network interfaces
+    serverAddr.sin_port = htons(5555);
+
     getch();
 }
